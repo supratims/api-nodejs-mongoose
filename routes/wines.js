017940f60e00e264166bf0e81c9886f8cb2ca49e
Supratim Chowdhury <supratims@gmail.com>
2016-10-08 10:57:18 +0100

    add comments and first set of APIs working

--- routes/wines.js
+++ routes/wines.js
@@ -1,3 +1,5 @@
+// Connection to mongodb. Could move this into a more central place
+
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/xshop');
 
@@ -7,7 +9,12 @@ db.once('open', function() {
   console.log('Connected to xshop database');
 });
 
+// Router code starts
+
+// First get a handle of our model
 var Wine = require('../models/wines');
+
+// Routes, that do real work to serve our APIs.
 
 exports.findAll = function(req, res){
   Wine.find({}, function (err, docs){
@@ -16,7 +23,7 @@ exports.findAll = function(req, res){
 }
 
 exports.addWine = function(req, res){
-  var wine = new Wine({ name: req.params.name, description: req.params.description});
+  var wine = new Wine({ name: req.body.name, description: req.body.description});
   wine.save(function (err, entity) {
     if (err) res.send({'error':err});
     res.send(entity);
--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 var express = require('express');
-
+var bodyParser = require('body-parser');
 var app = express();
-
+// Body parser is required for our apis to serve json and parse request
+app.use(bodyParser.json());
 var env = process.env.NODE_ENV || 'development';
 if ('development' == env) {
    // configure stuff here
 }
 
+// note: our router and model are named wines, perhaps not the greatest idea.
 var wines = require('./routes/wines');
+
+// Expose your APIs
 
 app.get('/wines', wines.findAll);
 
 app.post('/wines', wines.addWine);
 
+// Expose more APIs in future
 /*
 app.get('/wines/:id', wines.findById);
 app.post('/wines', wines.addProduct);
